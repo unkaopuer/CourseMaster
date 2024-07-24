fb980674a7e8910b41825646919c9f48c8e2cf08
unkaopuer <evelyn08201@gmail.com>
2024-07-24 14:27:40 -0700

    mern

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const authCtrl = require('../controllers/auth.controller')
 const router = express.Router()
 
-router.route('/auth/sighin')
+router.route('/auth/signin')
     .post(authCtrl.signin)
 router.route('/auth/signout')
     .post(authCtrl.signout)
