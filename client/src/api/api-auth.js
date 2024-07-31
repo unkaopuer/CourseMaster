cf60d4d46ded8a803ce4be94f3a081920659c0b5
unkaopuer <evelyn08201@gmail.com>
2024-07-31 15:05:10 -0700

    mern

--- client/src/api/api-auth.js
+++ client/src/api/api-auth.js
@@ -9,6 +9,10 @@ const signin = async(user)=>{
             credentials: 'include',
             body: JSON.stringify(user)
         })
+
+        if (!response.ok) {
+            throw new Error(`Signin failed: ${response.statusText}`);
+        }
         return await response.json()
     } catch(err) {
         console.log(err)
@@ -17,14 +21,56 @@ const signin = async(user)=>{
 
 const signout = async() =>{
     try{
-        let response = await fetch('/auth/signout',{ method:'GET'})
+        let response = await fetch('/auth/signout',{ 
+            method:'GET'
+        })
+        if (!response.ok) {
+            throw new Error(`Signout failed: ${response.statusText}`);
+        }
         return await response.json()
     } catch(err) {
-        console.log(err)
+        console.err(`Error during signout: ${err.message}`)
+        throw err
     }
+}
+
+const auth = {
+    isAuthenticated() {
+        if (typeof window == "undefined")
+            return false
+        if (sessionStorage.getItem('jwt'))
+            return JSON.parse(sessionStorage.getItem('jwt'))
+        else
+            return false
+    },
+    authenticate(jwt,cb){
+        if(typeof window !== "undefined")
+            sessionStorage.setItem('jwt',JSON.stringify(jwt))
+        cb()
+    },
+    clearJWT(cb){
+        if (typeof window !=="undefined")
+            sessionStorage.removeItem('jwt')
+        cb()
+        signout().then((date) => {
+            document.cookie = "t=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+        })
+    },
+    updateUser(user,cb){
+        if(typeof window !== "undefined"){
+            if(sessionStorage.getItem('jwt')){
+                let auth = JSON.parse(sessionStorage.getItem('jwt'))
+                auth.user = user
+                sessionStorage.setItem('jwt',JSON.stringify(auth))
+                cb()
+            }
+        }
+    }
+    
 }
 
 export{
     signin,
-    signout
+    signout,
+    auth
 }
